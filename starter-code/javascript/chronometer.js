5ed72ddd0dfc019a86d62aee3149c8b92d57213a
plsalles <plsalles@gmail.com>
2020-02-14 22:35:56 -0300

    Jasmin Done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,33 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    
+    this.intervalId = setInterval (() => {
+      this.currentTime++;
+      console.log(this.currentTime)
+    },1000);
+    
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
+  getSeconds() {
+    return Math.floor(this.currentTime%60);
+  }
+  twoDigitsNumber(num) {
+    if(num/10 < 1){
+    return '0'+num;
+    } else { return String(num);}
+  } 
+  stopClick() {}
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {}
 }
+
+let start = new Chronometer();
+
